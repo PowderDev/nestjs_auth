9215edcc7311e6357c4d07a87e1aa2532c08cb39
Makarov Timur <makarov-timyr@ya.ru>
2023-04-15 16:56:23 +0300

    fix: forgotten DATABASE_URL in env.d

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     JWT_ACCESS_SECRET: string;
     JWT_REFRESH_SECRET: string;
+    DATABASE_URL: string;
     REDIS_HOST: string;
     REDIS_PORT: string;
   }
